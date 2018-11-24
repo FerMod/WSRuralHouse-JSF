a13f2022be5f7820e19074c0184f8b5883fc259a
FerMod <tudelaigarcia@gmail.com>
2018-11-24 17:27:42 +0100

    Fix client panel

--- WebContent/resources/js/cp_utilities.js
+++ WebContent/resources/js/cp_utilities.js
@@ -6,8 +6,7 @@ function openTab(evt, tabName) {
 	}
 	tablinks = document.getElementsByClassName("tablinks");
 	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(
-				" active", "");
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
 	}
 	document.getElementById(tabName).style.display = "block";
 	evt.currentTarget.className += " active";
