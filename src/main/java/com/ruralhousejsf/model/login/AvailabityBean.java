9ec3f3d61dbdbb26d35cf7247961e00c954bd116
FosterGun <fostergun123@gmail.com>
2018-11-24 15:02:44 +0100

    Add bean for availabity cases

---
+++ src/main/java/com/ruralhousejsf/model/login/AvailabityBean.java
@@ -0,0 +1,32 @@
+package com.ruralhousejsf.model.login;
+
+import java.util.Date;
+
+public class AvailabityBean {
+	public Date startDate;
+	public Date endDate;
+	
+	public AvailabityBean() {
+	}
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+	
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	
+	public Date getEndDate() {
+		return endDate;
+	}
+	
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	public String establecer() {
+		return "lulz";
+	}
+	
+}
