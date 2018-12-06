68e56ea95f1f1c0fe4dbc68c82529a8e65355bca
FosterGun <fostergun123@gmail.com>
2018-12-06 21:44:53 +0100

    Fix views for hibernate utilities

---
+++ src/main/java/com/ruralhousejsf/model/availability/ControlBean.java
@@ -0,0 +1,15 @@
+package com.ruralhousejsf.model.availability;
+
+public class ControlBean {
+	public ControlBean() {
+	}
+	
+	public String controlSetAv() {
+		return "setav";
+	}
+	
+	public String controlQueryAv() {
+		return "queryav";
+	}
+	
+}
