742cb629beee37fa3cc71549d6bd54e1bf800fd1
FerMod <tudelaigarcia@gmail.com>
2018-12-11 22:33:16 +0100

    Refactor logger to another class and replace delete for truncate

--- src/main/java/com/ruralhousejsf/dataAccess/HibernateDataAccess.java
+++ src/main/java/com/ruralhousejsf/dataAccess/HibernateDataAccess.java
@@ -5,13 +5,11 @@ import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
+import com.ruralhousejsf.debug.ConsoleLogger;
 import com.ruralhousejsf.domain.Client;
 import com.ruralhousejsf.domain.Offer;
 import com.ruralhousejsf.domain.RuralHouse;
@@ -19,148 +17,161 @@ import com.ruralhousejsf.domain.util.ParseDate;
 
 public class HibernateDataAccess implements HibernateDataAccessInterface {
 	
-	private static final Level LOG_LEVEL = Level.ALL;
-	private static final String LOG_PATTERN = "[%-5p] [%d{dd/MM/yyyy HH:mm:ss}] %c %M - %m%n";
-	private static final Logger LOGGER = Logger.getLogger(HibernateDataAccess.class.getSimpleName());
+	private static final Logger LOGGER = ConsoleLogger.createLogger(HibernateDataAccess.class);
 
 	public static void main(String[] args) {
 		new HibernateDataAccess();
 	}
 
 	public HibernateDataAccess() {
-		initLogger();
 		initializeDB();
-	}
-	
-	private void initLogger() {
-		
-		// Setup basic configuration
-		BasicConfigurator.configure();
-		
-		// Set the logger pattern
-		PatternLayout layout = new PatternLayout(LOG_PATTERN);		
-		LOGGER.addAppender(new ConsoleAppender(layout));
-		
-		// Dont allow log propagation to parent loggers
-		LOGGER.setAdditivity(false);
-		
-		// Set the messages level
-		LOGGER.setLevel(LOG_LEVEL);
-		
 	}
 
 	public void initializeDB() {
-		
-		LOGGER.debug("Inicializando BD");
+
+		LOGGER.debug("Inicializando la BD");
 		
 		Session session = HibernateSession.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
+		LOGGER.trace("Sesion creada y empezada transaccion");
+
+		Query queryClient = session.createSQLQuery("TRUNCATE Client");
+		queryClient.executeUpdate();
+		LOGGER.trace("Tabla Client eliminada");
 		
-		@SuppressWarnings("unchecked")
-		List<Client> resultClient = session.createQuery("from Client").list();		
-		for (Client c : resultClient) {
-			session.delete(c);
-		}
-
-		@SuppressWarnings("unchecked")
-		List<Offer> resultOffer = session.createQuery("from Offer").list();
-		for (Offer o : resultOffer) {
-			session.delete(o);
-		}
-
-		@SuppressWarnings("unchecked")
-		List<RuralHouse> resultRH = session.createQuery("from RuralHouse").list();
-		for (RuralHouse rh : resultRH) {
-			session.delete(rh);
-		}
+		Query queryOffer = session.createSQLQuery("TRUNCATE Offer");
+		queryOffer.executeUpdate();
+		LOGGER.trace("Tabla Offer eliminada.");
+		
+		Query queryRuralHouse = session.createSQLQuery("TRUNCATE RuralHouse");
+		queryRuralHouse.executeUpdate();
+		LOGGER.trace("Tabla RuralHouse eliminada");
 
 		session.getTransaction().commit();
+		LOGGER.trace("Commit de los cambios y sesión cerrada");
 		LOGGER.debug("BD borrada");
 
 		createClient("cliente", "cliente123");
 		createClient("user", "user123");
 		createClient("paco", "paco123");
-
+		LOGGER.trace("Datos de la tabla Client creados");
+		
 		RuralHouse rh1 = createRuralHouse("Ezkioko etxea","Ezkio");
 		RuralHouse rh2 = createRuralHouse("Eskiatzeko etxea","Jaca");
 		RuralHouse rh3 = createRuralHouse("Udaletxea","Bilbo");
 		RuralHouse rh4 = createRuralHouse("Gaztetxea","Renteria");
+		LOGGER.trace("Datos de la tabla RuralHouse creados");
 
 		createOffer(rh1, LocalDate.of(2018, 12, 9), LocalDate.of(2018, 12, 11), 25.0);
 		createOffer(rh1, LocalDate.of(2018, 12, 12), LocalDate.of(2018, 12, 15), 25.0);
 		createOffer(rh2, LocalDate.of(2018, 12, 5), LocalDate.of(2018, 12, 10), 35.5);
 		createOffer(rh3, LocalDate.of(2019, 1, 3), LocalDate.of(2019, 1, 9), 40.0);
 		createOffer(rh4, LocalDate.of(2019, 1, 07), LocalDate.of(2019, 1, 21), 36.0);
+		LOGGER.trace("Datos de la tabla Offer creados");
 		
-		LOGGER.debug("BD inicializada");
+		LOGGER.debug("Inicializacion de la BD terminada");
+		
 	}
 
 	public RuralHouse createRuralHouse(String description, String city) {
+		
 		Session session = HibernateSession.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
+		
 		RuralHouse ruralHouse = new RuralHouse(description, city);
 		session.save(ruralHouse);
 		session.getTransaction().commit();
+		
 		LOGGER.debug(ruralHouse.toString() + " created.");
 		return ruralHouse;
 	}
-	
+
 	public Offer createOffer(RuralHouse ruralHouse, LocalDate firstDay, LocalDate lastDay, double price) {		
 		return createOffer(ruralHouse, ParseDate.asDate(firstDay), ParseDate.asDate(lastDay), price);
 	}
 
 	public Offer createOffer(RuralHouse ruralHouse, Date firstDay, Date lastDay, double price) {
+		
 		Session session = HibernateSession.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
+		
 		Offer offer = new Offer(ruralHouse, firstDay, lastDay, price);
 		session.save(offer);
-		LOGGER.debug(offer.toString() + " for " + ruralHouse.toString() + " created.");
+		
 		session.getTransaction().commit();
+		
+		LOGGER.debug(offer.toString() + " for " + ruralHouse.toString() + " created.");
 		return offer;
 	}
 
 	public Client createClient(String user, String pass) {
+		
 		Session session = HibernateSession.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
+		
 		Client client = new Client(user, pass);
 		session.save(client);
-		LOGGER.debug(client.toString() + " created.");
+		
 		session.getTransaction().commit();
+		
+		LOGGER.debug(client.toString() + " created.");
 		return client;
 	}
 
 	public List<RuralHouse> getAllRuralHouses() {
+		LOGGER.debug("getAllRuralHouses()");
+		
 		Session session = HibernateSession.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
+		
+		Query query = session.createQuery("FROM RuralHouse");
+		
 		@SuppressWarnings("unchecked")
-		List<RuralHouse> result = session.createQuery("from RuralHouse").list();
-		LOGGER.debug("getAllRuralHouses()");
+		List<RuralHouse> result = query.list();
+		
 		session.getTransaction().commit();
+		
 		return result;
 	}
-	
+
 	public List<Offer> getOffers(RuralHouse ruralHouse, LocalDate firstDay, LocalDate lastDay) {		
 		return getOffers(ruralHouse, ParseDate.asDate(firstDay), ParseDate.asDate(lastDay));
 	}
 
 	public List<Offer> getOffers(RuralHouse ruralHouse, Date firstDay, Date lastDay) {
+		LOGGER.debug("getOffers of " + ruralHouse.toString() + " with startDate " + firstDay.toString() + " and finalDate " + lastDay.toString());
+
 		Session session = HibernateSession.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
+
+		Query query = session.createQuery("FROM Offer WHERE firstDay >= ':firstDay' AND lastDay <= ':lastDay'");
+		
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		query.setParameter("firstDay", formatter.format(firstDay));
+		query.setParameter("lastDay", formatter.format(lastDay));
+		
 		@SuppressWarnings("unchecked")
-		List<Offer> result = session.createQuery("from Offer where firstDay >= '" + formatter.format(firstDay) + "' and lastDay <= '" + formatter.format(lastDay) + "'").list();
-		LOGGER.debug("getOffers of " + ruralHouse.toString() + " with startDate: " + firstDay.toString() + " and finalDate" + lastDay.toString());
+		List<Offer> result = query.list();
+		
 		session.getTransaction().commit();
+
 		return result;
 	}
-	
-	public List<Client> getClient(String user, String pass) {
+
+	public List<Client> getClient(String username, String password) {
+		LOGGER.debug("getUser " + username + " with password " + password + ".");
+		
 		Session session = HibernateSession.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
+		
+		Query query = session.createQuery("FROM Client WHERE username LIKE ':username' and password LIKE ':password'");
+		query.setParameter("username", username);
+		query.setParameter("password", password);
+		
 		@SuppressWarnings("unchecked")
-		List<Client> result = session.createQuery("from Client where username ='" + user + "' and password = '" + pass + "'").list();
-		LOGGER.debug("getUser " + user + " with password " + pass + ".");
+		List<Client> result = query.list();
 		session.getTransaction().commit();
+		
 		return result;
 	}
 
---
+++ src/main/java/com/ruralhousejsf/debug/ConsoleLogger.java
@@ -0,0 +1,46 @@
+package com.ruralhousejsf.debug;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+public final class ConsoleLogger {
+
+	/**
+	 * <code>OFF > FATAL > ERROR > WARN > INFO > DEBUG > TRACE > ALL</code>
+	 * <p>Log messages level priority. This means, if level is <code>WARN</code>, 
+	 * only <code>WARN</code>, <code>ERROR</code> and <code>FATAL</code> logs will show up.
+	 */
+	private static final Level ROOT_LOGGER_LEVEL = Level.OFF;
+	
+	private static final Level LOGGER_LEVEL = Level.ALL;
+	private static final String LOGGER_PATTERN = "[%-5p] [%d{dd/MM/yyyy HH:mm:ss}] %c %M - %m%n";
+	
+	private ConsoleLogger() {}
+	
+	public static Logger createLogger(Class<?> cls) {
+		
+		Logger logger = Logger.getLogger(cls.getSimpleName());
+
+		// Setup basic configuration
+		BasicConfigurator.configure();
+
+		// Set the logger pattern
+		PatternLayout layout = new PatternLayout(LOGGER_PATTERN);		
+		logger.addAppender(new ConsoleAppender(layout));
+
+		// Dont allow log propagation to parent loggers
+		logger.setAdditivity(false);
+		
+		// Set the message level. It will use the more restrictive level 
+		logger.setLevel(ROOT_LOGGER_LEVEL.isGreaterOrEqual(LOGGER_LEVEL) ? ROOT_LOGGER_LEVEL : LOGGER_LEVEL);
+		
+		// Set all loggers messages level
+		Logger.getRootLogger().setLevel(ROOT_LOGGER_LEVEL);
+		
+		return logger;
+	}
+	
+}
